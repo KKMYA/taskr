f9f3790fbba308317b2f02f7f5605b546b762164
KKMYA <alexis.callet@free.fr>
2025-03-24 17:13:04 +0100

    terms/login/create

--- task-manager-front/src/app/app.routes.ts
+++ task-manager-front/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
-import { TaskListComponent } from './components/task-list/task-list.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
+import { AccountCreationComponent } from './components/account-creation/account-creation.component';
+import { CguComponent } from './components/cgu/cgu.component';
 
 export const routes: Routes = [
     { path: '', component: HomepageComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'terms', component: CguComponent},
+    { path: 'create-account', component: AccountCreationComponent},
     { path: '**', redirectTo: '', pathMatch: 'full' } // Redirige toutes les URL inconnues
   ];
