c06b08f2e70bc33958b18bd320ee9885cd127f28
KKMYA <alexis.callet@free.fr>
2025-04-01 17:58:59 +0200

    entities / repo

--- task-manager-front/src/app/components/login/login.component.ts
+++ task-manager-front/src/app/components/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  standalone: false,
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule], 
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
@@ -13,7 +14,6 @@ export class LoginComponent {
   loginForm;
 
   constructor(private readonly fb: FormBuilder, private readonly authService: AuthService) {
-    // Initialisation de loginForm dans le constructeur
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
@@ -21,21 +21,23 @@ export class LoginComponent {
   }
 
   onSubmit() {
-    if (this.loginForm.valid) {
-      const { username, password } = this.loginForm.value;
 
-      if (username && password) {
-        this.authService.login({ username, password }).subscribe(
-          (response) => {
-            console.log('User logged in successfully', response);
-          },
-          (error) => {
-            console.log('Login failed', error);
-          }
-        );
-      } else {
-        console.log('Invalid form data');
-      }
+    const username = this.loginForm.get('username')?.value;
+    const password = this.loginForm.get('password')?.value;
+      
+    if (typeof username === 'string' && typeof password === 'string') {
+      this.authService.login({ username, password }).subscribe(
+        (response) => {
+          console.log('User logged in successfully', response);
+        },
+        (error) => {
+          console.log('Login failed', error);
+        }
+      );
+    } else {
+      console.log('Invalid form data');
+    }
+      
     }
   }
-}
+  
