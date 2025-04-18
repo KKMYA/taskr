ecd3ade53e2c64db88a0e6113046be0b7ab033c6
KKMYA <alexis.callet@free.fr>
2025-04-18 12:14:18 +0200

    Homepage tasks display

--- task-manager-front/src/app/components/homepage/homepage.component.ts
+++ task-manager-front/src/app/components/homepage/homepage.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../services/task.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss'
+  styleUrl: './homepage.component.scss',
+  imports: [CommonModule]
 })
 export class HomepageComponent implements OnInit {
   tasks: any[] = [];
